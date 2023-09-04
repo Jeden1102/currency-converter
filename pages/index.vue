4f404d461ea7948dac34080a06ad8a8d8eb48947
Jeden1102 <bum123@o2.pl>
2023-09-04 20:59:36 +0200

    master. Init app + base dependecies installerd

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="bg-red-200">test</h1>
+    <div class="box w-20 h-20 bg-red-300"></div>
+    <div class="box-2 w-[130px] h-[130px] bg-green-300">
+      {{ indexStore.isMenuOverlay }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useIndexStore } from "../store/index";
+const indexStore = useIndexStore();
+</script>
